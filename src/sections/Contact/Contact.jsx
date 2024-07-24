ffc2d71e5e4abc8f809a40217def18368c05c3f5
Dan-Muchiri <danspmunene@gmail.com>
2024-07-24 22:30:48 +0300

    Added formspree for email

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <section id="contact" className={styles.container}>
         <h1 className="sectionTitle">Contact</h1>
-        <form action="">
+        <form action="https://formspree.io/f/xwpevaqk" method="post">
             <div className="formGroup">
                 <label htmlFor="name" hidden>
                     Name
