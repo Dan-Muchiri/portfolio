a1430c5e94b4d1b0025275cf28cf772182e5b6df
Dan-Muchiri <danspmunene@gmail.com>
2024-09-24 14:40:56 +0300

    Added resume

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import linkedinLight from "../../assets/linkedin-light.svg"
 import linkedinDark from "../../assets/linkedin-dark.svg"
 import githubLight from "../../assets/github-light.svg"
 import githubDark from "../../assets/github-dark.svg"
-import CV from "../../assets/Full-stack-resume.pdf"
+import CV from "../../assets/Dan-Muchiri-Resume.pdf"
 import { useTheme } from "../../common/ThemeContext"
 
 function Hero() {
