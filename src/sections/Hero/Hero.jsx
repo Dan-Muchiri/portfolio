7bc4924646d20c94d4ff88b3381d20f17a88d062
Dan-Muchiri <danspmunene@gmail.com>
2025-05-27 20:38:50 +0300

    Updated resume

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import linkedinLight from "../../assets/linkedin-light.svg"
 import linkedinDark from "../../assets/linkedin-dark.svg"
 import githubLight from "../../assets/github-light.svg"
 import githubDark from "../../assets/github-dark.svg"
-import CV from "../../assets/Dan-Muchiri-Resume.pdf"
+import CV from "../../assets/Dan-Muchiri-Resume (2).pdf"
 import { useTheme } from "../../common/ThemeContext"
 
 function Hero() {
